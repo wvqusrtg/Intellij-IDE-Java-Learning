ab1dc62b29ea3060448f6765172a5f0a1a0617b9
倪新生 <1911398892@qq.com>
2016-05-25 17:36:30 +0800

    ‘IntellijIDEAjavastudy’

---
+++ hello_world/src/com/asiainfo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.asiainfo;
+
+/**
+ * com.asiainfo
+ * hello_world
+ * 16/5/25 | 下午4:54
+ * Created by nixinsheng
+ * Email:1911398892@qq.com
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Start!");
+    }
+}
